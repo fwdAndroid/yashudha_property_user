571186f1f4496ac3a3e7d5858c6811d1d91a77e3
fwdAndroid <fwdkaleem@gmail.com>
2023-03-05 07:47:34 +0500

    Splash Completed

---
+++ lib/splash/second_splash_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SecondSplashScreen extends StatefulWidget {
+  const SecondSplashScreen({super.key});
+
+  @override
+  State<SecondSplashScreen> createState() => _SecondSplashScreenState();
+}
+
+class _SecondSplashScreenState extends State<SecondSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 100),
+            child: Text(
+              "Find best place \nto stay in good price ",
+              style: TextStyle(fontSize: 25, color: Color(0xff204D6C)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10, top: 100, right: 10),
+            child: Stack(
+              children: [
+                Image.asset(
+                  "assets/Rectangle 6.png",
+                ),
+                Positioned(
+                  left: 80,
+                  top: 400,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (builder) => SecondSplashScreen()));
+                    },
+                    child: Text(
+                      "Next",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xfff8BC83F),
+                        fixedSize: Size(190, 60)),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:yashudha_property_user/splash/second_splash_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,6 +13,37 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        image: DecorationImage(
+          image: AssetImage(
+            "assets/image 3.png",
+          ),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/Group 75.png"),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (builder) => SecondSplashScreen()));
+            },
+            child: Text(
+              "Lets Start",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xfff8BC83F), fixedSize: Size(190, 60)),
+          )
+        ],
+      ),
+    ));
   }
 }
